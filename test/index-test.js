5918424c99f662c9d5663657f81fb7a5f454e384
Carlo Fernando <ca.fernando415@gmail.com>
2018-09-08 08:32:30 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -131,7 +131,7 @@ describe("removeFromCart()", function() {
     const secondItemName = getCart()[1].itemName;
 
     expect(firstItemName).toEqual("vanilla");
-    expect(secondItemName).toEqual("yams");
+    expect(secondItemName).toEqual("watermelon");
 
     removeFromCart("yams");
 
