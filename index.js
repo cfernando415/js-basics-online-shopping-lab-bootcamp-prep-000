0f3e05895fecbfbb481e4f41927fd525c2834c63
Carlo Fernando <ca.fernando415@gmail.com>
2018-09-08 08:15:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,9 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  
+  for (let i = 0; i < cart.length; i++) {
+    item.toEquals()
+  }
 }
 
 function placeOrder(cardNumber) {
