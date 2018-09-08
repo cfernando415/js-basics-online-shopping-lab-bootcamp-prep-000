8c0def7598f280901d13ddcbef1d9cb8bfc4f4bd
Carlo Fernando <ca.fernando415@gmail.com>
2018-09-08 09:03:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber === undefined || cardNumber === null) {
-    Console.log("Sorry, we don't have a credit card on file for you.");
+    return "Sorry, we don't have a credit card on file for you.";
   } else {
     var totalCost = total();
     cart.splice(0, cart.length);
